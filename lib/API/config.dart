a8242146d62ab140601c0cf0d62eb74040de2464
Hamyu00 <seikyusiang@gmail.com>
2021-06-25 18:33:30 +0800

    update

--- lib/API/config.dart
+++ lib/API/config.dart
@@ -1,12 +1,7 @@
 class Config {
-  // String url = 'https://app.infinityrobot.net/';
-  // String url2 = 'app.infinityrobot.net';
-
-  
   String url = 'https://bosco.greatwallsolution.com/';
   String url2 = 'bosco.greatwallsolution.com';
 
-  // String url = 'https://saetest.greatwallsolution.com/';
-  // String url2 = 'saetest.greatwallsolution.com';
-  
+  // String url = 'https://app.infinityrobot.net/';
+  // String url2 = 'app.infinityrobot.net';
 }
